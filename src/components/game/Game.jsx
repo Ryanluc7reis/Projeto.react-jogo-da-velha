9be69d724a3c3de50e9c76e49f1df602469684d1
Ryan Lucas <ryanlucasfrfrfrfr@hotmail.com>
2022-11-07 20:49:46 -0300

    Criado status no gameOption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,20 +1,29 @@
-
+import { useState } from 'react'
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
+/*
+Um array com varias posição de forma simplificada
+let array = Array(9).fill(0)
+   *quantas posição tem *       /qual posição começa
+*/
 
 function Game () {
+  const [gameState,setGameState] = useState(Array(9).fill(0))
+  
+  
+  console.log(gameState)
   return (
     <div className={styles.game}>
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
+      {
+        gameState.map((value ,pos) =>
+         
+         <GameOption 
+            key={`game-option-pos-${pos}`}
+            status={value}
+         /> )
+      }
+     
     </div>
 
   )
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,20 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption () {
+const GameIcon = ({iconName }) => <Icon iconName={iconName } size ="30px" />
+
+function GameOption ({status}) {
   return (
     <div className={styles.gameOption}>
-     
+      {
+        status === 1 && <GameIcon iconName="circle" size = '30px' />
+
+      }
+       {
+        status === -1 && <GameIcon iconName="x" size = '30px' />
+
+      }
+    
     </div>
   )
 }
